60eee75bc3a1501c6d74b0745b8143656df286ed
Mac <denpef@gmail.com>
2018-10-16 10:12:15 +0300

    pods

---
+++ Converter/Application/Utility/AmountField.swift
@@ -0,0 +1,51 @@
+//
+//  AmountField.swift
+//  Converter
+//
+//  Created by Денис Ефимов on 11.10.2018.
+//  Copyright © 2018 Denis Efimov. All rights reserved.
+//
+
+import UIKit
+
+extension AmountTextField {
+    struct Appearance {
+        var normalColor = UIColor.lightGray.withAlphaComponent(0.6)
+        var selectedColor = UIColor.black
+        var lineHeight: CGFloat = 2
+    }
+}
+
+class AmountTextField: UITextField {
+    private let appearance: Appearance
+
+    private lazy var lineView: UIView = {
+        let lineView = UIView()
+        lineView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
+        lineView.isUserInteractionEnabled = false
+        lineView.frame.size.height = appearance.lineHeight
+        lineView.backgroundColor = appearance.normalColor
+        return lineView
+    }()
+
+    init(appearance: Appearance = Appearance()) {
+        self.appearance = appearance
+        super.init(frame: .zero)
+        addSubview(lineView)
+        textColor = appearance.normalColor
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func toggleColors() {
+        if isFirstResponder {
+            lineView.backgroundColor = appearance.selectedColor
+            textColor = appearance.selectedColor
+        } else {
+            lineView.backgroundColor = appearance.normalColor
+            textColor = appearance.normalColor
+        }
+    }
+}
